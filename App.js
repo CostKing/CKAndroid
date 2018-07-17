116d066525a1852f660f9135ded2ddf7f7e78fa3
kavin1990 <40630661+kavin1990@users.noreply.github.com>
2018-07-17 12:18:54 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+import { AppRegistry,StyleSheet,Text ,View, Image } from 'react-native';
+import Login from './node_modules/src/components/login/Login';
+
+export default class k1 extends Component {
+  render() {
+    return (
+     <Login/>
+     
+
+    );
+  }
+}
