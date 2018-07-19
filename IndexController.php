0a338a1248aa7b2fa95e0320ec8bb1c905be92e2
kavin1990 <40630661+kavin1990@users.noreply.github.com>
2018-07-19 12:17:49 +0530

    Add files via upload

--- IndexController.php
+++ IndexController.php
@@ -2,13 +2,53 @@
 class Magentotutorial_Helloworld_IndexController extends
 Mage_Core_Controller_Front_Action  {
 
-public function indexAction()
-{
-$json = json_decode(file_get_contents('php://input'),true);
-$mail = $json['email'];
-$pass = $json['password'];
-echo $mail;
-echo $pass;
+
+
+public function indexAction(){
+/**
+ * Example of products list retrieve using Customer account via Magento REST API. OAuth authorization is used
+ */
+$callbackUrl = "http://yourhost/oauth_customer.php";
+$temporaryCredentialsRequestUrl = "http://magentohost/oauth/initiate?oauth_callback=" . urlencode($callbackUrl);
+$adminAuthorizationUrl = 'http://magentohost/oauth/authorize';
+$accessTokenRequestUrl = 'http://magentohost/oauth/token';
+$apiUrl = 'http://magentohost/api/rest';
+$consumerKey = '85c4087a008f4c4efdd3bd20f375617f';
+$consumerSecret = '41c18e438e60055b5617c116b65e14649';
+
+session_start();
+if (!isset($_GET['oauth_token']) && isset($_SESSION['state']) && $_SESSION['state'] == 1) {
+    $_SESSION['state'] = 0;
 }
+try {
+    $authType = ($_SESSION['state'] == 2) ? OAUTH_AUTH_TYPE_AUTHORIZATION : OAUTH_AUTH_TYPE_URI;
+    $oauthClient = new OAuth($consumerKey, $consumerSecret, OAUTH_SIG_METHOD_HMACSHA1, $authType);
+    $oauthClient->enableDebug();
+
+    if (!isset($_GET['oauth_token']) && !$_SESSION['state']) {
+        $requestToken = $oauthClient->getRequestToken($temporaryCredentialsRequestUrl);
+        $_SESSION['secret'] = $requestToken['oauth_token_secret'];
+        $_SESSION['state'] = 1;
+        header('Location: ' . $adminAuthorizationUrl . '?oauth_token=' . $requestToken['oauth_token']);
+        exit;
+    } else if ($_SESSION['state'] == 1) {
+        $oauthClient->setToken($_GET['oauth_token'], $_SESSION['secret']);
+        $accessToken = $oauthClient->getAccessToken($accessTokenRequestUrl);
+        $_SESSION['state'] = 2;
+        $_SESSION['token'] = $accessToken['oauth_token'];
+        $_SESSION['secret'] = $accessToken['oauth_token_secret'];
+        header('Location: ' . $callbackUrl);
+        exit;
+    } else {
+        $oauthClient->setToken($_SESSION['token'], $_SESSION['secret']);
+        $resourceUrl = "$apiUrl/products";
+        $oauthClient->fetch($resourceUrl, array(), 'GET', array('Content-Type' => 'application/json', 'Accept' => '*/*'));
+        $productsList = json_decode($oauthClient->getLastResponse());
+        print_r($productsList);
+    }
+} catch (OAuthException $e) {
+    print_r($e);
 }
+
+}}
 ?>
